d3577f50840864bca083c179ac02e5abf04a47e5
EkouNaylamp <115675532+EkouNaylamp@users.noreply.github.com>
2026-04-14 21:21:23 -0500

    Update database URL to use environment variable

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,6 +5,6 @@ export default defineConfig({
   schema: "./drizzle/schema.ts",
   out: "./drizzle",
   dbCredentials: {
-    url: "postgresql://postgres:root@localhost:5432/sga_db",
+    url: process.env.DATABASE_URL,
   },
 });
